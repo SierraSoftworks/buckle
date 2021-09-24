cb017d68ae274cb8dc931025099e0dd67a4fe890
Benjamin Pannell <benjamin@pannell.dev>
2021-09-24 23:17:48 +0100

    fix: Use the correct span name for package.apply

--- src/commands/apply.rs
+++ src/commands/apply.rs
@@ -49,7 +49,7 @@ impl CommandRunnable for ApplyCommand {
         let packages = crate::core::package::get_all_packages(&config_dir.join("packages"))?;
 
         for package in packages {
-            let _span = info_span!("package.plan", "package.id"=%package.id).entered();
+            let _span = info_span!("package.apply", "package.id"=%package.id).entered();
 
             writeln!(output, "")?;
             writeln!(output, " + package '{}'", &package.id)?;
